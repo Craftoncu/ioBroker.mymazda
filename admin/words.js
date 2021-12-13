6faf38510be5e8e7271b6e1d9964dee89e71deee
Daniel Kollmannsberger <daniel@kollmannsberger.de>
2021-12-13 09:04:31 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'mymazda adapter settings': {
+		'en': 'Adapter settings for mymazda',
+		'de': 'Adaptereinstellungen für mymazda',
+		'ru': 'Настройки адаптера для mymazda',
+		'pt': 'Configurações do adaptador para mymazda',
+		'nl': 'Adapterinstellingen voor mymazda',
+		'fr': "Paramètres d'adaptateur pour mymazda",
+		'it': "Impostazioni dell'adattatore per mymazda",
+		'es': 'Ajustes del adaptador para mymazda',
+		'pl': 'Ustawienia adaptera dla mymazda',
+		'zh-cn': 'mymazda的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'option1',
+		'ru': 'option1',
+		'pt': 'option1',
+		'nl': 'option1',
+		'fr': 'option1',
+		'it': 'option1',
+		'es': 'option1',
+		'pl': 'option1',
+		'zh-cn': 'option1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'option2',
+		'ru': 'option2',
+		'pt': 'option2',
+		'nl': 'option2',
+		'fr': 'option2',
+		'it': 'option2',
+		'es': 'option2',
+		'pl': 'option2',
+		'zh-cn': 'option2'
+	}
+};
